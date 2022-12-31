33b9fba5f7db11a955b158bfa34308b26df6eb66
Evgeniy <gerardcobain1@gmail.com>
2022-12-31 15:31:42 +0200

    Виконання 2 завдання

---
+++ src/js/02-timer.js
@@ -0,0 +1,72 @@
+import flatpickr from 'flatpickr';
+
+import 'flatpickr/dist/flatpickr.min.css';
+let selected = '';
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  input: document.querySelector('#datetime-picker'),
+  days: document.querySelector('.value[data-days]'),
+  hours: document.querySelector('.value[data-hours]'),
+  minutes: document.querySelector('.value[data-minutes]'),
+  seconds: document.querySelector('.value[data-seconds]'),
+};
+refs.startBtn.disabled = true;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    selected = selectedDates[0];
+    if (selected < Date.now()) {
+      window.alert('Please choose a date in the future');
+    } else {
+      refs.startBtn.disabled = false;
+    }
+  },
+};
+
+refs.startBtn.addEventListener('click', onStartClick);
+const calendar = flatpickr(refs.input, options);
+
+function startTimer() {
+  const ms = selected - Date.now();
+  const timeToEnd = convertMs(ms);
+  updateClockface(timeToEnd);
+}
+function onStartClick(evt) {
+  refs.startBtn.disabled = true;
+  const timeId = setInterval(startTimer, 1000);
+}
+
+function updateClockface({ days, hours, minutes, seconds }) {
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.minutes.textContent = `${minutes}`;
+  refs.seconds.textContent = `${seconds}`;
+}
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
+}
